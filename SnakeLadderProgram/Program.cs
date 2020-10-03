e0c04cf20d381e4aeeabf2f999acda28ef75b44e
Sudipta <sudiptabala32@gmail.com>
2020-10-03 08:06:08 +0530

    [Sudipta] Refactor : Rolling the Die and displaying the number

--- SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/Program.cs
@@ -4,6 +4,13 @@ namespace Snake_And_Ladder
 {
     class Program
     {
+        //Returns Output Of Die Roll
+        public static int rollTheDie()
+        {
+            Random random = new Random();
+            int numberOnDie = random.Next(1, 7);
+            return numberOnDie;
+        }
         static void Main(string[] args)
         {
             //Welcome msg
