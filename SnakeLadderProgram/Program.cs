993665673432c089718a1cb6bbee0e501dc2e5c0
Sudipta <sudiptabala32@gmail.com>
2020-10-03 08:11:06 +0530

    [Sudipta] Refactor : Checking the option

--- SnakeLadderProgram/Program.cs
+++ SnakeLadderProgram/Program.cs
@@ -4,12 +4,37 @@ namespace Snake_And_Ladder
 {
     class Program
     {
+        public const int NoPlay = 0;
+        public const int Ladder = 1;
+        public const int Snake = 2;
         //Returns Output Of Die Roll
         public static int rollTheDie()
         {
             Random random = new Random();
             int numberOnDie = random.Next(1, 7);
             return numberOnDie;
+        }
+        //Returns the forward or the backward movement of the player
+        public static int newPosition(int numberOnDie)
+        {
+            int toAdd = 0;
+            Random random = new Random();
+            int toMove = random.Next(0, 3);
+            switch (toMove)
+            {
+                case NoPlay:
+                    Console.WriteLine("No Play");
+                    break;
+                case Ladder:
+                    toAdd = numberOnDie;
+                    Console.WriteLine("Ladder so moving forward.");
+                    break;
+                case Snake:
+                    toAdd = -numberOnDie;
+                    Console.WriteLine("Snake so moving backward!");
+                    break;
+            }
+            return toAdd;
         }
         static void Main(string[] args)
         {
